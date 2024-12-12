16f666f06067f8eda0f6e111858befcc20185424
FLAVIO GOMES DE LANNA <132150354+fglanna@users.noreply.github.com>
2024-12-11 21:32:00 -0300

    Inclusão Mapa, id 3º commit

---
+++ src/app/market/[id].tsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { View, Alert } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+import { Loading } from "@/components/loading";
+
+import { api } from "@/services/api";
+
+export default function Market() {
+
+    const [data, setData] = useState()
+    const [isLoading, setIsLoading] = useState(true)
+
+    const params = useLocalSearchParams<{ id: string }>()
+
+    async function fetchMarket() {
+        try {
+            const { data } = await api.get(`./markets/${params.id}`)
+            setData(data)
+            setIsLoading(false)
+
+        } catch (error) {
+            console.log(error)
+            Alert.alert("Erro", "Não foi possível carregar os dados", [
+                {
+                    text: "OK",
+                    onPress: () => router.back()
+                },
+            ])
+
+        }
+
+    }
+
+    useEffect(() => {
+        fetchMarket()
+    }, [params.id])
+
+    if (isLoading) {
+        return <Loading />
+    }
+
+    return <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+
+
+    </View>
+
+}
